f564c3fc70761c4137b239a575f36f7e90916f65
Enes <1eneskaraman@gmail.com>
2022-07-06 21:32:56 +0300

    enes test yapildi

--- src/Grup19.java
+++ src/Grup19.java
@@ -2,5 +2,6 @@ public class Grup19 {
     public static void main(String[] args) {
         System.out.println("merhaba arkadaslar");
         System.out.println("test");
+        System.out.println("Test ENES");
     }
 }
